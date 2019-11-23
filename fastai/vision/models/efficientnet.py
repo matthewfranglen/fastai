58ee09ff2f12838a11712364909bc6c931ae8b95
Matthew Franglen <matthew@franglen.org>
2019-11-23 20:06:34 +0000

    Add json mapping and start trying to make this compliant

--- fastai/vision/models/efficientnet.py
+++ fastai/vision/models/efficientnet.py
@@ -1,53 +1,61 @@
 from ...core import *
+from pathlib import Path
 from torch import nn
 from torch.utils import model_zoo
 
 __all__ = ['efficientnetb0','efficientnetb1','efficientnetb2','efficientnetb3','efficientnetb4','efficientnetb5','efficientnetb6','efficientnetb7']
 
 
-def _efficientnet(arch: EfficientNetParameters, blocks: List[EfficientNetBlockParameters], pretrained: bool, progress: bool):
-    model = EfficientNet(blocks, arch)
+def _efficientnet(arch: str, blocks: List[EfficientNetBlockParameters], pretrained: bool, progress: bool):
+    params = _MODEL_PARAMS[arch]
+    model = EfficientNet(blocks, params)
     if pretrained:
+        mapping = json.reads((Path(__file__).parent/'_efficientnet-mapping.json').read_text())[arch]
         state_dict = model_zoo.load_url(arch.url, progress=progress)
+        state_dict = {mapping[k]: v for k, v in state_dict.items()}
         model.load_state_dict(state_dict)
     return model
 
 def efficientnetb0(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b0'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b0'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
 def efficientnetb1(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b1'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b1'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
 def efficientnetb2(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b2'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b2'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
 def efficientnetb3(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b3'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b3'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
 def efficientnetb4(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b4'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b4'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
 def efficientnetb5(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b5'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b5'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
 def efficientnetb6(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b6'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b6'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
 def efficientnetb7(pretrained=False, progress=True, **kwargs):
-    return _efficientnet(arch=EFFICIENTNET_PARAMETERS['efficientnet-b7'], blocks=EFFICIENTNET_BLOCK_PARAMETERS, pretrained=pretrained, progress=progress)
+    return _efficientnet(arch=_MODEL_PARAMS['efficientnet-b7'], blocks=_BLOCK_PARAMS, pretrained=pretrained, progress=progress)
 
+
+Value2d = Union[int, Tuple[int, int]]
 
 class EfficientNet(nn.Sequential):
-    def __init__(self, blocks: List[EfficientNetBlockParameters], global_parameters: EfficientNetParameters):
+    """ https://arxiv.org/pdf/1905.11946.pdf """
+
+    def __init__(self, blocks: List[EfficientNetBlockParameters], model_params: EfficientNetParameters):
         pcn = partial(
-            pad_conv_norm,
-            image_size=global_parameters.image_size,
-            batch_norm_momentum=1 - global_parameters.batch_norm_momentum,
-            batch_norm_epsilon=global_parameters.batch_norm_epsilon,
+            pad_conv_bn,
+            image_size=model_params.image_size,
+            batch_norm_momentum=1 - model_params.batch_norm_momentum,
+            batch_norm_epsilon=model_params.batch_norm_epsilon,
         )
 
-        in_channels = 3  # rgb
-        out_channels = round_filters(32, global_parameters)
+        in_channels = 3
+        out_channels = round_filters(32, model_params)
 
         layers = []
         layers.append(
@@ -56,48 +64,43 @@ class EfficientNet(nn.Sequential):
 
         block_layers = []
         block_count = sum(max(1, block.num_repeat) for block in blocks)
-        for block_parameters in blocks:
+        for block in blocks:
             # Update block input and output filters based on depth multiplier.
-            block_parameters = block_parameters.clone(
-                input_filters=round_filters(block_parameters.input_filters, global_parameters),
-                output_filters=round_filters(block_parameters.output_filters, global_parameters),
-                num_repeat=round_repeats(block_parameters.num_repeat, global_parameters),
+            block = block.clone(
+                input_filters=round_filters(block.input_filters, model_params),
+                output_filters=round_filters(block.output_filters, model_params),
+                num_repeat=round_repeats(block.num_repeat, model_params),
             )
 
             # The first block needs to take care of stride and filter size increase.
-            block_layers.append(mirb(block_parameters, global_parameters))
-            if block_parameters.num_repeat > 1:
-                block_parameters = block_parameters.clone(input_filters=block_parameters.output_filters, stride=1)
-            for _ in range(block_parameters.num_repeat - 1):
-                if global_parameters.drop_connect_rate:
-                    drop_connect_rate = global_parameters.drop_connect_rate * len(block_layers) / block_count
-                else:
-                    drop_connect_rate = 0
-                block_layers.append(
-                    mirb(block_parameters, global_parameters, drop_connect_rate=drop_connect_rate)
-                )
+            block_layers.append(mbconv(block, model_params))
+            if block.num_repeat > 1:
+                block = block.clone(input_filters=block.output_filters, stride=1)
+            for _ in range(block.num_repeat - 1):
+                dropout = model_params.drop_connect_rate * len(block_layers) / block_count if dropout else 0
+                block_layers.append(mbconv(block, model_params, dropout=dropout))
         layers.append(nn.Sequential(*block_layers))
 
-        in_channels = block_parameters.output_filters  # output of final block
-        out_channels = round_filters(1280, global_parameters)
+        in_channels = block.output_filters
+        out_channels = round_filters(1280, model_params)
         layers.append(
             pcn(in_channels=in_channels, out_channels=out_channels, kernel_size=1, bias=False)
         )
-        layers.append(EfficientNetHead(out_channels, global_parameters))
+        layers.append(EfficientNetHead(out_channels, model_params))
 
         super().__init__(*layers)
 
 
-def mirb(block_parameters: EfficientNetBlockParameters, global_parameters: EfficientNetParameters, drop_connect_rate: float = 0) -> nn.Module:
-    """ Mobile Inverted Residual Bottleneck Block """
+def mbconv(block_parameters: EfficientNetBlockParameters, model_params: EfficientNetParameters, dropout: float = 0) -> nn.Module:
+    """ Mobile Inverted Bottleneck with Squeeze and Excitation """
     layers = []
     output_channels = block_parameters.input_filters * block_parameters.expand_ratio
 
     pcn = partial(
-        pad_conv_norm,
-        image_size=global_parameters.image_size,
-        batch_norm_momentum=1 - global_parameters.batch_norm_momentum,
-        batch_norm_epsilon=global_parameters.batch_norm_epsilon,
+        pad_conv_bn,
+        image_size=model_params.image_size,
+        batch_norm_momentum=1 - model_params.batch_norm_momentum,
+        batch_norm_epsilon=model_params.batch_norm_epsilon,
     )
 
     if block_parameters.expand_ratio != 1:
@@ -105,32 +108,29 @@ def mirb(block_parameters: EfficientNetBlockParameters, global_parameters: Effic
             pcn(in_channels=block_parameters.input_filters, out_channels=output_channels, kernel_size=1, bias=False)
         )
 
-    # Depthwise convolution phase
     layers.append(
         pcn(
             in_channels=output_channels,
             out_channels=output_channels,
-            groups=output_channels,  # groups makes it depthwise
+            groups=output_channels,
             kernel_size=block_parameters.kernel_size,
             stride=block_parameters.stride,
             bias=False,
         )
     )
 
-    # Squeeze and Excitation layer, if desired
     if block_parameters.se_ratio is not None and 0 < block_parameters.se_ratio <= 1:
         layers.append(
             SqueezeAndExcitation(block_parameters=block_parameters, output_channels=output_channels)
         )
 
-    # Output phase
     layers.append(
         pcn(in_channels=output_channels, out_channels=block_parameters.output_filters, kernel_size=1, bias=False, has_swish=False)
     )
 
     if block_parameters.id_skip and block_parameters.stride == 1 and block_parameters.input_filters == block_parameters.output_filters:
-        if drop_connect_rate:
-            return SkipDropConnection(drop_connect_rate, *layers)
+        if dropout:
+            return SkipDropConnection(dropout, *layers)
         return SkipConnection(*layers)
     return nn.Sequential(*layers)
 
@@ -148,13 +148,13 @@ class SqueezeAndExcitation(nn.Sequential):
                 in_channels=output_channels,
                 out_channels=num_squeezed_channels,
                 kernel_size=1,
-            ),  # squeeze
+            ),
             MemoryEfficientSwish(),
             nn.Conv2d(
                 in_channels=num_squeezed_channels,
                 out_channels=output_channels,
                 kernel_size=1,
-            ),  # expand
+            ),
         )
 
     def forward(self, _in, *others):
@@ -177,27 +177,11 @@ class SkipDropConnection(nn.Sequential):
         out = super().forward(_in, *others)
         return F.dropout(_in, p=self.dropout, training=self.training) + out
 
-def pad(
-    conv: nn.Conv2d, image_size: Union[int, Tuple[int, int]]
-) -> Optional[nn.Module]:
-    kernel_size = conv.weight.shape[-2:]
-    return _padding_module(
-        stride=conv.stride,
-        dilation=conv.dilation,
-        kernel_size=kernel_size,
-        image_size=image_size,
-    )
 
-
-def _padding_module(  # pylint: disable=too-many-locals
-    stride: Union[int, Tuple[int, int]],
-    dilation: Union[int, Tuple[int, int]],
-    kernel_size: Union[int, Tuple[int, int]],
-    image_size: Union[int, Tuple[int, int]],
-) -> Optional[nn.Module]:
-    stride_height, stride_width = listify(stride, 2)
-    dilation_height, dilation_width = listify(dilation, 2)
-    kernel_height, kernel_width = listify(kernel_size, 2)
+def pad(conv: nn.Conv2d, image_size: Value2d) -> Optional[nn.Module]:
+    stride_height, stride_width = listify(conv.stride, 2)
+    dilation_height, dilation_width = listify(conv.dilation, 2)
+    kernel_height, kernel_width = conv.weight.shape[-2:]
     image_height, image_width = listify(image_size, 2)
 
     output_height = math.ceil(image_height / stride_height)
@@ -225,9 +209,7 @@ def _padding_module(  # pylint: disable=too-many-locals
     return None
 
 
-def _calculate_padding(
-    _in: int, out: int, stride: int, dilation: int, kernel: int
-) -> Tuple[int, int]:
+def _calculate_padding(_in: int, out: int, stride: int, dilation: int, kernel: int) -> int:
     return max((out - 1) * stride + (kernel - 1) * dilation + 1 - _in, 0)
 
 
@@ -249,10 +231,8 @@ class MemoryEfficientSwish(nn.Module):
     def forward(self, _in, *_):
         return SwishImplementation.apply(_in)
 
-Value2d = Union[int, Tuple[int, int]]
 
-
-def pad_conv_norm(  # pylint: disable=too-many-arguments
+def pad_conv_bn(
     *,
     image_size: Value2d,
     batch_norm_momentum: float,
@@ -299,15 +279,15 @@ def pad_conv_norm(  # pylint: disable=too-many-arguments
     return nn.Sequential(*layers)
 
 class EfficientNetHead(nn.Module):
-    def __init__(self, in_channels: int, global_parameters: EfficientNetParameters):
+    def __init__(self, in_channels: int, model_params: EfficientNetParameters):
         super().__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.dropout = nn.Dropout(global_parameters.dropout_rate)
-        self.fc = nn.Linear(  # pylint: disable=invalid-name
-            in_channels, global_parameters.num_classes
+        self.dropout = nn.Dropout(model_params.dropout_rate)
+        self.fc = nn.Linear(
+            in_channels, model_params.num_classes
         )
 
-    def forward(self, _in):  # pylint: disable=arguments-differ
+    def forward(self, _in):
         batch_size = _in.shape[0]
         out = self.avg_pool(_in)
         out = out.view(batch_size, -1)
@@ -315,14 +295,13 @@ class EfficientNetHead(nn.Module):
         return self.fc(out)
 
 
-def round_filters(filters: int, global_parameters: EfficientNetParameters) -> int:
+def round_filters(filters: int, model_params: EfficientNetParameters) -> int:
     """ Calculate and round number of filters based on depth multiplier. """
-    multiplier = global_parameters.width_coefficient
-    if not multiplier:
+    if not model_params.width_coefficient:
         return filters
-    divisor = global_parameters.depth_divisor
-    min_depth = global_parameters.min_depth
-    filters *= multiplier
+    divisor = model_params.depth_divisor
+    min_depth = model_params.min_depth
+    filters *= model_params.width_coefficient
     min_depth = min_depth or divisor
     new_filters = max(min_depth, int(filters + divisor / 2) // divisor * divisor)
     if new_filters < 0.9 * filters:  # prevent rounding by more than 10%
@@ -330,14 +309,13 @@ def round_filters(filters: int, global_parameters: EfficientNetParameters) -> in
     return int(new_filters)
 
 
-def round_repeats(repeats: int, global_parameters: EfficientNetParameters) -> int:
+def round_repeats(repeats: int, model_params: EfficientNetParameters) -> int:
     """ Round number of filters based on depth multiplier. """
-    multiplier = global_parameters.depth_coefficient
-    if not multiplier:
+    if not model_params.depth_coefficient:
         return repeats
-    return int(math.ceil(multiplier * repeats))
+    return int(math.ceil(model_params.depth_coefficient * repeats))
 
-@dataclass(frozen=True)  # pylint: disable=too-many-instance-attributes
+@dataclass(frozen=True)
 class EfficientNetParameters:
     url: str
 
@@ -359,7 +337,7 @@ class EfficientNetParameters:
         return EfficientNetParameters(**parameters)
 
 
-@dataclass(frozen=True)  # pylint: disable=too-many-instance-attributes
+@dataclass(frozen=True)
 class EfficientNetBlockParameters:
     kernel_size: int
     num_repeat: int
@@ -376,7 +354,7 @@ class EfficientNetBlockParameters:
         return EfficientNetBlockParameters(**parameters)
 
 
-EFFICIENTNET_PARAMETERS = {
+_MODEL_PARAMS = {
     "efficientnet-b0": EfficientNetParameters(
         url="http://storage.googleapis.com/public-models/efficientnet/efficientnet-b0-355c32eb.pth",
         width_coefficient=1.0,
@@ -435,7 +413,7 @@ EFFICIENTNET_PARAMETERS = {
     ),
 }
 
-EFFICIENTNET_BLOCK_PARAMETERS = [
+_BLOCK_PARAMS = [
     EfficientNetBlockParameters(
         kernel_size=3,
         num_repeat=1,
@@ -515,7 +493,7 @@ def load_pretrained_weights(model: nn.Module, model_name: str) -> None:
     state = {
         mapping[key]: value
         for key, value in model_zoo.load_url(
-            EFFICIENTNET_PARAMETERS[model_name].url
+            _MODEL_PARAMS[model_name].url
         ).items()
     }
     model.load_state_dict(state)
